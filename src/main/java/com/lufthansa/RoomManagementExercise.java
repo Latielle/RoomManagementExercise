e6bdd506efaea66bdbe0dc07bef856df490cf55f
Latielle <47073900+Latielle@users.noreply.github.com>
2019-01-27 16:25:43 +0300

    q
    q

--- src/main/java/com/lufthansa/RoomManagementExercise.java
+++ src/main/java/com/lufthansa/RoomManagementExercise.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSpringJpacrudApplication {
+public class RoomManagementExercise {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSpringJpacrudApplication.class, args);
+		SpringApplication.run(RoomManagementExercise.class, args);
 	}
 
 }
--- src/main/java/com/lufthansa/model/Room.java
+++ src/main/java/com/lufthansa/model/Room.java
@@ -7,8 +7,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Size;
-
-import org.hibernate.annotations.NaturalId;
 import org.hibernate.validator.constraints.Range;
 
 @Entity
@@ -19,12 +17,10 @@ public class Room {
 	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	 private long id;
 	 
-	 //@NaturalId (mutable = false)
 	 @Size (min=1,max=1, message = "Only one character allowed  ")
 	 @Column(name="building")
 	 private String building;
 	 
-	 //@NaturalId (mutable = false)
 	 @Range(min=1, max=999, message = "Room number must between 1 and 999")
 	 @Column(name="roomNumber")
 	 private int roomNumber;
